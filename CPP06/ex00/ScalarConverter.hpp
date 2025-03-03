d763e969343d13003b37d57fb074751680b55874
lilitsimon <lilitsimonyan94@gmail.com>
2025-03-03 14:47:27 +0100

    added cpp06

---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,20 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <limits>
+#include <cstdlib>
+
+class ScalarConverter {
+    private:
+    ScalarConverter();
+    ScalarConverter(const ScalarConverter& other);
+    ScalarConverter& operator=(const ScalarConverter& other);
+    ~ScalarConverter();
+
+};
+
+#endif
